60e9b51bab3a435fd1a64f4e5bd98c8f897828c1
amani2409 <einkauf-a@posteo.de>
2024-06-05 21:01:07 +0200

    small steps!

---
+++ functions.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Button:
+    def __init__(self, color, x, y, width, height, text, font, fontcolor, fontsize, hover_color):
+        button = Button(color, x, y, width, height, text, font)
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.font = font
+        self.color = color
+        self.fontcolor = fontcolor
+        self.fontsize = fontsize
+        self.hover_color = hover_color
+
+    def draw(self, screen):
+        mouse = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse):
+            pygame.draw.rect(screen, self.hover_color, self.rect)
+        else:
+            pygame.draw.rect(screen, self.color, self.rect)
+
+
+    # def update(self, screen):
