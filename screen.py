f98ef3873948684896d8a19db4512863ddaab580
amani2409 <einkauf-a@posteo.de>
2024-06-27 18:02:59 +0200

    little bit design - font size and position

--- screen.py
+++ screen.py
@@ -1,6 +1,6 @@
 import pygame
 
-from classes.variables import WIDTH, HEIGHT, WHITE
+from classes.variables import WIDTH, HEIGHT, WHITE, FONT, FONT_SM
 from database import get_user, add_user, show_all_user_highscore
 
 
@@ -12,18 +12,28 @@ def draw_startScreen(user_data):
     screen.fill((0, 0, 0))
 
     pygame.display.set_caption('Start Menu')
-    font = pygame.font.SysFont('Arial', 50)
-    title = font.render(f'Hallo {user_data['username']}', True, (255, 255, 255))
-    highscore = font.render(f'Highscore: {user_data['highscore']}', True, (255, 255, 255))
-    all_highscore = font.render('Show all Highscore [h]', True, (255, 255, 255))
-    start_button = font.render('Start [s]', True, (255, 255, 255))
-    quit_button = font.render('Quit [q]', True, (255, 255, 255))
+
+    title = FONT.render(f'Hallo {user_data['username']}', True, WHITE)
+    title_rect = title.get_rect(center=(WIDTH / 2, HEIGHT / 4))
+
+    highscore = FONT.render(f'Highscore: {user_data['highscore']}', True, WHITE)
+    highscore_rect = highscore.get_rect(center=(WIDTH / 2, HEIGHT / 3))
+
+    all_highscore = FONT_SM.render('Show all Highscore [h]', True, WHITE)
+    all_highscore_rect = all_highscore.get_rect(center=(WIDTH / 2, HEIGHT / 2))
+
+    start_button = FONT_SM.render('Start [s]', True, WHITE)
+    start_button_rect = start_button.get_rect(center=(WIDTH / 2, HEIGHT / 2 + 100))
+
+    quit_button = FONT_SM.render('Quit [q]', True, WHITE)
+    quit_button_rect = quit_button.get_rect(center=(WIDTH / 2, HEIGHT / 2 + 150))
+
     screen.blit(background, (0, 0))  # blit wird genutzt um eine Sache auf einen andere Sache/Oberfläche zu zeichnen
-    screen.blit(title, (WIDTH / 2 - title.get_width() / 2, HEIGHT / 3 - title.get_height() / 2))
-    screen.blit(highscore, (WIDTH / 2 - highscore.get_width() / 2, HEIGHT / highscore.get_height() / 2))
-    screen.blit(start_button, (WIDTH / 2 - start_button.get_width() / 2, HEIGHT / 2 - start_button.get_height() / 2))
-    screen.blit(all_highscore, (WIDTH / 2 - all_highscore.get_width() / 2, HEIGHT / 2 + start_button.get_height() / 2))
-    screen.blit(quit_button, (WIDTH / 2 - quit_button.get_width() / 2, HEIGHT / 4 + start_button.get_height()))
+    screen.blit(title, title_rect)
+    screen.blit(highscore, highscore_rect)
+    screen.blit(all_highscore, all_highscore_rect)
+    screen.blit(start_button, start_button_rect)
+    screen.blit(quit_button, quit_button_rect)
     pygame.display.update()
 
 
@@ -35,8 +45,7 @@ def draw_highscore_screen():
     screen.blit(background, (0, 0))
 
     pygame.display.set_caption('Highscore')
-    font = pygame.font.SysFont('Arial', 50)
-    title = font.render('Highscores Top 10', True, WHITE)
+    title = FONT.render('Highscores Top 10', True, WHITE)
     title_rect = title.get_rect(center=(WIDTH / 2, 50))
     screen.blit(title, title_rect)
 
@@ -45,11 +54,11 @@ def draw_highscore_screen():
     dis = 100
     for i, data in enumerate(all_highscores):
         text = f'{i + 1}. {data[0]}: {data[1]}'
-        text_surface = font.render(text, True, WHITE)
+        text_surface = FONT_SM.render(text, True, WHITE)
         screen.blit(text_surface, (WIDTH / 2 - text_surface.get_width() / 2, dis))
         dis += 50
 
-    back_button = font.render('Back [b]', True, WHITE)
+    back_button = FONT_SM.render('Back [b]', True, WHITE)
     back_button_rect = back_button.get_rect(topright=(WIDTH - 10, 10))
     screen.blit(back_button, back_button_rect.topleft)
 
@@ -60,12 +69,14 @@ def draw_endScreen():
     background = pygame.transform.scale(background, (WIDTH, HEIGHT))
     screen = pygame.display.get_surface()
     screen.fill((0, 0, 0))
+
     pygame.display.set_caption('End Screen')
-    font = pygame.font.SysFont('Arial', 50)
-    title = font.render('GameOver', True, (255, 255, 255))
-    restart_button = font.render('Restart [r]', True, (255, 255, 255))
-    home_button = font.render('Home Screen [h]', True, (255, 255, 255))
-    quit_button = font.render('Quit [q]', True, (255, 255, 255))
+
+    title = FONT.render('GameOver', True, (255, 255, 255))
+    restart_button = FONT_SM.render('Restart [r]', True, (255, 255, 255))
+    home_button = FONT_SM.render('Home Screen [h]', True, (255, 255, 255))
+    quit_button = FONT_SM.render('Quit [q]', True, (255, 255, 255))
+
     screen.blit(background, (0, 0))
     screen.blit(title, (WIDTH / 2 - title.get_width() / 2, HEIGHT / 4 - title.get_height() / 2))
     screen.blit(restart_button,
@@ -82,12 +93,11 @@ def draw_nextLevel():
     screen.fill((0, 0, 0))
 
     pygame.display.set_caption('Winning Screen')
-    font = pygame.font.SysFont('Arial', 50)
-    title = font.render('Won: You made it!', True, (255, 255, 255))
-    restart_button = font.render('Restart [r]', True, (255, 255, 255))
-    next_button = font.render('next Level [n]', True, (255, 255, 255))
-    home_button = font.render('Home Screen [h]', True, (255, 255, 255))
-    quit_button = font.render('Quit [q]', True, (255, 255, 255))
+    title = FONT.render('Won: You made it!', True, (255, 255, 255))
+    restart_button = FONT_SM.render('Restart [r]', True, (255, 255, 255))
+    next_button = FONT_SM.render('next Level [n]', True, (255, 255, 255))
+    home_button = FONT_SM.render('Home Screen [h]', True, (255, 255, 255))
+    quit_button = FONT_SM.render('Quit [q]', True, (255, 255, 255))
     screen.blit(background, (0, 0))
     screen.blit(title, (WIDTH / 2 - title.get_width() / 2, HEIGHT / 4 - title.get_height() / 2))
     screen.blit(restart_button,
@@ -103,18 +113,22 @@ def draw_loginScreen():
     background = pygame.transform.scale(background, (WIDTH, HEIGHT))
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('Login')
-    font = pygame.font.SysFont('Arial', 50)
-    label_font = pygame.font.SysFont('Arial', 30)
-    title = font.render('Enter your username and password!', True, WHITE)
-    enter_button = font.render('Login [press Enter]!', True, WHITE)
+    # font = pygame.font.SysFont('Arial', 50)
+    # label_font = pygame.font.SysFont('Arial', 30)
+    title = FONT.render('Enter your username and password!', True, WHITE)
+    enter_button = FONT.render('Login [press Enter]!', True, WHITE)
     input_username = pygame.Rect(WIDTH / 2 - 100, HEIGHT / 2 - 60, 200, 40)
     input_passwort = pygame.Rect(WIDTH / 2 - 100, HEIGHT / 2 + 20, 200, 40)
 
-    username_label = label_font.render('Username:', True, WHITE)
-    password_label = label_font.render('Password:', True, WHITE)
+    username_label = FONT_SM.render('Username:', True, WHITE)
+    password_label = FONT_SM.render('Password:', True, WHITE)
 
-    color_active = pygame.Color(255, 255, 255)
-    color_inactive = pygame.Color(0, 0, 0)
+    color_active = pygame.Color(30, 160, 15)
+    color_inactive = pygame.Color(255, 255, 255)
+
+    color_user = color_inactive
+    color_pw = color_inactive
+
 
     active_username = False
     active_password = False
@@ -134,13 +148,20 @@ def draw_loginScreen():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if input_username.collidepoint(event.pos):
                     active_username = True
+                    color_user = color_active
                     active_password = False
+                    color_pw = color_inactive
+
                 elif input_passwort.collidepoint(event.pos):
                     active_username = False
+                    color_user = color_inactive
                     active_password = True
+                    color_pw = color_active
                 else:
                     active_username = False
+                    color_user = color_inactive
                     active_password = False
+                    color_pw = color_inactive
 
             if event.type == pygame.KEYDOWN:
                 if active_username:
@@ -168,12 +189,13 @@ def draw_loginScreen():
                                 username = ''
                                 password = ''
 
+
         screen.fill((30, 30, 30))
         screen.blit(background, (0, 0))
         screen.blit(title, (WIDTH / 2 - title.get_width() / 2, HEIGHT / 4 - title.get_height() / 2))
 
-        draw_username = label_font.render(username, True, WHITE)
-        draw_passwort = font.render('*' * len(password), True, WHITE)
+        draw_username = FONT_SM.render(username, True, WHITE)
+        draw_passwort = FONT.render('*' * len(password), True, WHITE)
 
         input_username.w = max(200, draw_username.get_width() + 10)
         input_passwort.w = max(200, draw_passwort.get_width() + 10)
@@ -184,14 +206,14 @@ def draw_loginScreen():
         screen.blit(username_label, (input_username.x, input_username.y - 32))
         screen.blit(password_label, (input_passwort.x, input_passwort.y - 32))
 
-        pygame.draw.rect(screen, WHITE, input_username, 2)
-        pygame.draw.rect(screen, WHITE, input_passwort, 2)
+        pygame.draw.rect(screen, color_user, input_username, 2)
+        pygame.draw.rect(screen, color_pw, input_passwort, 2)
 
         screen.blit(enter_button,
                     (WIDTH / 2 - enter_button.get_width() / 2, input_passwort.y + input_passwort.height + 20))
 
         if error_message:
-            error_label = label_font.render(error_message, True, (255, 0, 0))
+            error_label = FONT_SM.render(error_message, True, (255, 0, 0))
             screen.blit(error_label,
                         (WIDTH / 2 - error_label.get_width() / 2, input_passwort.y + input_passwort.height + 70))
 
