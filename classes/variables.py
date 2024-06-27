9eed9c3c5415f39c8c95dc1f2313ce751475bcea
amani2409 <einkauf-a@posteo.de>
2024-06-27 18:02:59 +0200

    little bit design - font size and position

--- classes/variables.py
+++ classes/variables.py
@@ -1,6 +1,12 @@
+import pygame
+pygame.init()
+
 WIDTH = 1000
 HEIGHT = 600
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+
+FONT = pygame.font.SysFont('Arial', 50)
+FONT_SM = pygame.font.SysFont('Arial', 30)
 
